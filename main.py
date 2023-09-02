97a7a1693d94d9e0b0c28eaccbaee84c7c82f8a4
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-09-01 22:08:50 -0400

    Handle invalid city names

--- main.py
+++ main.py
@@ -2,28 +2,47 @@ import streamlit as st
 import plotly.express as px
 from data import get_weather_data
 
+
+# Add page title , input boxes, slider and selectbox
 st.title("Weather Forecast")
 
 place = st.text_input(label="Place:", placeholder="enter a place")
-days = st.slider(label="Forecast Days", min_value=1, max_value=5,
+days_count = st.slider(label="Forecast Days", min_value=1, max_value=5,
                  step=1, help='select the number of days')
 
 option = st.selectbox("Select weather info to view",
                       ["Temperature", "Sky"], )
-print(option, days)
-
-st.subheader(f"{option} for the next {days} days in {place}")
 
 
-# def get_weather_data(_days):
-#     dates = ["2023-25-08", "2023-26-08", "2023-27-08"]
-#     temperatures = [12, 20, 16]
-#     temperatures = [days * temp for temp in temperatures]
-#     return dates, temperatures
+subheader_text = f"{option} for the next {days_count} days in {place}"
+st.subheader(subheader_text)
 
 
-d, t = get_weather_data(days)
 
-figure = px.line(x=d, y=t,
-                 labels={"x": "Date", "y": "Temperatures"})
-st.plotly_chart(figure_or_data=figure)
+if place:
+    # Get temperaure/sky data
+    try:
+         filtered_data = get_weather_data(place, days_count)
+  
+         match option:
+            case 'Temperature':
+                temperatures = [result['main']['temp']
+                                for result in filtered_data]
+                dates = [result['dt_txt'] for result in filtered_data]
+                figure = px.line(x=dates, y=temperatures,
+                                labels={"x": "Date", "y": "Temperatures"})
+                st.plotly_chart(figure_or_data=figure)
+        
+            case 'Sky':
+                images ={
+                    'Clear':'images/clear.png',
+                    'Clouds':'images/cloud.png',
+                    'Rain': 'images/rain.png',
+                    'Snow':'images/snow.png'
+                }
+                sky_conditons = [result['weather'][0]['main']
+                                for result in filtered_data]
+                images_paths=[images[condition] for condition in sky_conditons]
+                st.image(images_paths, width=115)
+    except KeyError:
+        st.write('Invalid City name')
